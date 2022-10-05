1aa6e7fd591b9cb381977f93c14bfba6b4251755
Saeryhz <william@rockett.fr>
2022-10-05 12:09:02 +0200

    Update playerMovement.js

--- src/handlers/playerMovement.js
+++ src/handlers/playerMovement.js
@@ -2,7 +2,9 @@ import constants from '../constants/index.js';
 
 export default function playerMovement(player) {
     onKeyDown(constants.keyboard.INPUT_FORWARD, () => {
-        player.move(0, -constants.game.CAR_SPEED);
+        if (player.pos.y > player.width / 2) {
+            player.move(0, -constants.game.CAR_SPEED);
+        }
     });
 
     onKeyDown(constants.keyboard.INPUT_LEFT, () => {
@@ -10,7 +12,9 @@ export default function playerMovement(player) {
     });
 
     onKeyDown(constants.keyboard.INPUT_BACKWARD, () => {
-        player.move(0, constants.game.CAR_SPEED);
+        if (player.pos.y < height() - player.width / 2) {
+            player.move(0, constants.game.CAR_SPEED);
+        }
     });
 
     onKeyDown(constants.keyboard.INPUT_RIGHT, () => {
